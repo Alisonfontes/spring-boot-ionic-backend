f4932a3f2780f9f299934e79e4dbea3d09e0dee8
Alison Fontes <sistema.fontes@gmail.com>
2021-03-17 00:41:20 -0300

    Put, Delete e Get para Clientes

--- src/main/java/com/alisonshow/cursomc/services/ClienteService.java
+++ src/main/java/com/alisonshow/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.alisonshow.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alisonshow.cursomc.domain.Cliente;
+import com.alisonshow.cursomc.dto.ClienteDTO;
 import com.alisonshow.cursomc.repositories.ClienteRepository;
+import com.alisonshow.cursomc.services.exceptions.DataIntegrityException;
 import com.alisonshow.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,6 +27,47 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado ID: " + id + ", Tipo:  " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	//delete
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			// TODO: handle exception
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+					
+			
+		}
+		
+	}
+	
+	// chamando todas as categorias
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	// paginacao
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	// validação da categoria apartir de um dto
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(),objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
