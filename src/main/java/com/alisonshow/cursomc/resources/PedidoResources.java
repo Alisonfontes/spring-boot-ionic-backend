c1ff42dffab37964373a2fad282fad04d163ea1c
Alison Fontes <sistema.fontes@gmail.com>
2021-03-24 23:02:39 -0300

    Inserindo Pedido

--- src/main/java/com/alisonshow/cursomc/resources/PedidoResources.java
+++ src/main/java/com/alisonshow/cursomc/resources/PedidoResources.java
@@ -2,14 +2,22 @@ package com.alisonshow.cursomc.resources;
 
 
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.alisonshow.cursomc.domain.Categoria;
 import com.alisonshow.cursomc.domain.Pedido;
+import com.alisonshow.cursomc.dto.CategoriaDTO;
 import com.alisonshow.cursomc.services.PedidoService;
 
 @RestController
@@ -26,6 +34,15 @@ public class PedidoResources {
 		
 		return ResponseEntity.ok().body(obj);
 		
-		
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
--- src/main/java/com/alisonshow/cursomc/services/PedidoService.java
+++ src/main/java/com/alisonshow/cursomc/services/PedidoService.java
@@ -1,12 +1,20 @@
 package com.alisonshow.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.alisonshow.cursomc.domain.ItemPedido;
+import com.alisonshow.cursomc.domain.PagamentoComBoleto;
 import com.alisonshow.cursomc.domain.Pedido;
+import com.alisonshow.cursomc.domain.enums.EstadoPagamento;
+import com.alisonshow.cursomc.repositories.ItemPedidoRepository;
+import com.alisonshow.cursomc.repositories.PagamentoRepository;
 import com.alisonshow.cursomc.repositories.PedidoRepository;
+import com.alisonshow.cursomc.repositories.ProdutoRepository;
 import com.alisonshow.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,6 +24,21 @@ public class PedidoService {
 	private PedidoRepository repo;
 	// buscando categoria pro codigo
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		
@@ -24,6 +47,28 @@ public class PedidoService {
 		}
 		*/
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado ID: " + id + ", Tipo:  " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preecherPagamentoComBoleto(pagto, obj.getInstante());
+			
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
